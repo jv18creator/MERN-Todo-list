f98b45b6b8907b4a9c414c82ed7e47962c95338f
Jeet Viramgama <jviramgama5@gmail.com>
2022-03-14 05:59:24 +0000

    filter data using comparison query operator

--- backend/connection.js
+++ backend/connection.js
@@ -8,20 +8,28 @@ async function main() {
   try {
     // Connect to the MongoDB cluster
     await client.connect();
-    console.log("client established", client);
+    // console.log("client established", client);
 
     // create a function that lists avaialable databases
     // await listDatabases(client);
 
     // create new airbnb listing
-    await createListing(client, {
-      name: "Lovely Loft",
-      summary: "A charming loft in Paris",
-      bedrooms: 1,
-      bathrooms: 1,
-    });
+    // await createListing(client, {
+    //   name: "Lovely Loft",
+    //   summary: "A charming loft in Paris",
+    //   bedrooms: 1,
+    //   bathrooms: 1,
+    // });
 
-    // Make the appropriate DB calls
+    // Read Single Doc
+    // await findOneListingByName(client, "Lovely Loft");
+
+    // Read filtered doc
+    await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
+      minimumNumberOfBedrooms: 4,
+      minimumNumberOfBathrooms: 2,
+      maximumNumberOfResults: 5,
+    });
   } catch (err) {
     console.log("error occured", err);
   } finally {
@@ -53,4 +61,63 @@ async function createListing(client, newListing) {
   console.log(
     `New listing created with the following id: ${result.insertedId}`
   );
+}
+
+// Read single document by name
+async function findOneListingByName(client, nameOfListing) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .findOne({ name: nameOfListing });
+
+  if (result) {
+    console.log(
+      `Found a listing in the collection with the name '${nameOfListing}':`
+    );
+    console.log(result);
+  } else {
+    console.log(`No listings found with the name '${nameOfListing}'`);
+  }
+}
+
+// Read filtered data
+async function findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(
+  client,
+  {
+    minimumNumberOfBedrooms = 0,
+    minimumNumberOfBathrooms = 0,
+    maximumNumberOfResults = Number.MAX_SAFE_INTEGER,
+  } = {}
+) {
+  const cursor = client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .find({
+      bedrooms: { $gte: minimumNumberOfBedrooms },
+      bathrooms: { $gte: minimumNumberOfBathrooms },
+    })
+    .sort({ last_review: -1 })
+    .limit(maximumNumberOfResults);
+
+  const results = await cursor.toArray();
+
+  if (results.length > 0) {
+    console.log(
+      `Found listing(s) with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms:`
+    );
+    results.forEach((result, i) => {
+      const date = new Date(result.last_review).toDateString();
+
+      console.log();
+      console.log(`${i + 1}. name: ${result.name}`);
+      console.log(`   _id: ${result._id}`);
+      console.log(`   bedrooms: ${result.bedrooms}`);
+      console.log(`   bathrooms: ${result.bathrooms}`);
+      console.log(`   most recent review date: ${date}`);
+    });
+  } else {
+    console.log(
+      `No listings found with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`
+    );
+  }
 }
